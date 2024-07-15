51609e00cf0297159368c43505fe983f5e068a29
syaz-1 <83575979+syaz-1@users.noreply.github.com>
2024-07-15 09:24:40 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
+from mysql_helper import run_mysql_queries
 import streamlit as st
+from streamlit import connections
+conn = st.connection('mysql', type='sql')
+# df = conn.query('select * from vectordb.demo_embeddings', ttl=600)
 
-st.title("🎈 My new app")
+st.title("🎈 MySQL GEN ai Test")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+question = st.text_area("Enter you question:")
+if st.button("Ask"):
+    query=f"set @query='{question}'; SET @options = JSON_Object('vector_store', JSON_ARRAY('vectordb.demo_embedding')); call sys.ML_RAG(@query,@output,@options); SELECT JSON_UNQUOTE(JSON_EXTRACT(@output, '$.text'));"
+
+    # answer=conn.query("select * from vectordb")
+    answer=run_mysql_queries(query)
+    if answer is not None:
+        st.write(answer[0])
+    else:
+        st.write("DB connection error")
